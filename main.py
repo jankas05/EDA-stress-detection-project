c7db78829b4df6fc83d1bdec7edf6528305c4c05
Jan Kasierski <jankas05@mail.uni-paderborn.de>
2025-12-28 11:51:34 +0100

    Restructured the train_model method

--- main.py
+++ main.py
@@ -52,16 +52,14 @@ def prepare_data(df, random_state):
     Prepares the data for classification. Drops all NaN rows, separates the 
     infodata and the classification feature.
 
-    arguments: \n
-    df - panda dataframe \n
-    random_state - number to reproduce or randomize splitting
+    :param df: panda dataframe \n
+    :param random_state: number to reproduce or randomize splitting
 
-    returns:\n
-    X_train - panda dataframe of all other relevant features for training \n
-    y_train - panda dataframe with the classification feature for training\n
-    X_test  - panda dataframe of all other relevant features for testing \n
-    y_test  - panda dataframe with the classification feature for testing \n
-    groups  - array with the group indices for cross-validation
+    :return X_train: panda dataframe of all other relevant features for training \n
+    :return y_train: panda dataframe with the classification feature for training\n
+    :return X_test:  panda dataframe of all other relevant features for testing \n
+    :return y_test:  panda dataframe with the classification feature for testing \n
+    :return groups:  array with the group indices for cross-validation
     """
 
     #basic preprocessing
@@ -103,6 +101,8 @@ def train_model(X_train, y_train, groups, model_select, random_state ):
     :param model_select: The machine learning algorithm upon which the data will be trained. Has to be 
     either "knn", "svm", "nb", "lr" or "rf"
     :param random_state: int to add reproducability to the results
+
+    :return best_model: The trained model
     """
     unique_subjects = len(groups.unique())
     train_size = (unique_subjects - 1) / unique_subjects 
@@ -111,31 +111,17 @@ def train_model(X_train, y_train, groups, model_select, random_state ):
     match model_select:
         case "knn": #K Nearest Neighbors
             #initialize the model and the hyperparameter grid
-            knc = KNeighborsClassifier(algorithm="auto", leaf_size=30, metric="minkowski", 
+            predefined_model = KNeighborsClassifier(algorithm="auto", leaf_size=30, metric="minkowski", 
                                        n_neighbors=5, p=2, weights="uniform")
             param_grid={
                 "n_neighbors":  [3,5,7,9,11,13,15],
                 "weights": ["uniform", "distance"],
             }
+            model = KNeighborsClassifier()
 
-            #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
-            prefitting_scores = ms.cross_val_score(knc, X_train, y_train, groups=groups, cv=gss)
-            print("Prefitting Scores:", prefitting_scores)
-            gs = ms.RandomizedSearchCV(estimator=KNeighborsClassifier(), 
-                                       param_distributions=param_grid, n_iter=30, cv=gss, 
-                                       scoring='accuracy', n_jobs=-1, random_state=random_state)
-            gs.fit(X_train, y_train, groups=groups)
-
-            #train the 'best' estimator and calculate postifitting scores
-            best_knc = gs.best_estimator_
-            best_knc.fit(X_train, y_train)
-            postfitting_scores = ms.cross_val_score(best_knc, X_train, y_train, groups=groups, cv=gss)
-            print("Postfitting Scores:", postfitting_scores)
-            return best_knc
-        
         case "svm": #Support Vector Machines
             #initilize the model and hyperparameter grid
-            svc = SVC(C=1.0, cache_size=1024, class_weight=None, coef0=0.0, 
+            predefined_model = SVC(C=1.0, cache_size=1024, class_weight=None, coef0=0.0, 
                       decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf', 
                       max_iter=-1, probability=True, random_state=random_state, 
                       shrinking=True, tol=0.001, verbose=False )
@@ -144,74 +130,31 @@ def train_model(X_train, y_train, groups, model_select, random_state ):
                 "kernel": ["linear", "rbf", "poly"],
                 "class_weight": [None, "balanced"],
             }
-
-            #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
-            prefitting_scores = ms.cross_val_score(svc, X_train, y_train, groups=groups, cv=gss)
-            print("Prefitting Scores:", prefitting_scores)
-            gs = ms.RandomizedSearchCV(estimator=SVC(random_state=random_state), 
-                                       param_distributions=param_grid, n_iter=30, cv=gss, 
-                                       scoring='accuracy', n_jobs=-1, random_state=random_state)
-            gs.fit(X_train, y_train, groups=groups)
-
-            #train the 'best' estimator and calculate postifitting scores
-            best_svc = gs.best_estimator_
-            best_svc.fit(X_train, y_train)
-            postfitting_scores = ms.cross_val_score(best_svc, X_train, y_train, groups=groups, cv=gss)
-            print("Postfitting Scores:", postfitting_scores)
-            return best_svc
+            model = SVC(random_state=random_state)
         
         case "nb": #Naive Bayes
             #initilize the model and hyperparameter grid
-            nb = GaussianNB(priors=None, var_smoothing=1e-09)
+            predefined_model = GaussianNB(priors=None, var_smoothing=1e-09)
             param_grid={
                 "var_smoothing": [1e-11, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4],
             }
-
-            #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
-            prefitting_scores = ms.cross_val_score(nb, X_train, y_train, groups=groups, cv=gss)
-            print("Prefitting Scores:", prefitting_scores)
-            gs = ms.RandomizedSearchCV(estimator=GaussianNB(), 
-                                       param_distributions=param_grid, n_iter=30, cv=gss, 
-                                       scoring='accuracy', n_jobs=-1, random_state=random_state)
-            gs.fit(X_train, y_train, groups=groups)
-
-            #train the 'best' estimator and calculate postifitting scores
-            best_nb = gs.best_estimator_
-            best_nb.fit(X_train, y_train)
-            postfitting_scores = ms.cross_val_score(best_nb, X_train, y_train, groups=groups, cv=gss)
-            print("Postfitting Scores:", postfitting_scores)
-            return best_nb
+            model = GaussianNB()
 
         case "lr": #Logistic Regression
             #initilize the model and hyperparameter grid
-            lr = LogisticRegression(C=1.0, class_weight=None,dual=False, fit_intercept=True, 
-                                    intercept_scaling=1, max_iter=100, n_jobs=-1, penalty='l2'
-                                    ,random_state=random_state, solver='liblinear',tol=0.0001, 
+            predefined_model = LogisticRegression(C=1.0, class_weight=None,dual=False, fit_intercept=True, 
+                                    intercept_scaling=1, max_iter=100,random_state=random_state, solver='liblinear',tol=0.0001, 
                                     verbose=0, warm_start=False)
             param_grid={
                 "C": [0.01, 0.1, 1.0, 10.0, 100.0],
                 "class_weight": [None, "balanced"],
 
             }
-            
-            #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
-            prefitting_scores = ms.cross_val_score(lr, X_train, y_train, groups=groups, cv=gss)
-            print("Prefitting Scores:", prefitting_scores)
-            gs = ms.RandomizedSearchCV(estimator=LogisticRegression(random_state=random_state), 
-                                       param_distributions=param_grid, n_iter=30, cv=gss, 
-                                       scoring='accuracy', n_jobs=-1, random_state=random_state)
-            gs.fit(X_train, y_train, groups=groups)
-
-            #train the 'best' estimator and calculate postifitting scores
-            best_lr = gs.best_estimator_
-            best_lr.fit(X_train, y_train)
-            postfitting_scores = ms.cross_val_score(best_lr, X_train, y_train, groups=groups, cv=gss)
-            print("Postfitting Scores:", postfitting_scores)
-            return best_lr
+            model = LogisticRegression(random_state=random_state)
 
         case "rf": #RandomForest
             #initialize the model and the hyperparameter grid
-            rf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=None, 
+            predefined_model = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=None, 
                                         max_leaf_nodes=None, min_impurity_decrease=0.0, 
                                         min_samples_leaf=1, min_samples_split=2, 
                                         min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1, oob_score=False, 
@@ -222,33 +165,33 @@ def train_model(X_train, y_train, groups, model_select, random_state ):
                 "min_samples_leaf": [1, 2, 5, 10],
                 "max_features": ["sqrt", "log2", 0.3, 0.5],
             }
-
-            #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
-            prefitting_scores = ms.cross_val_score(rf, X_train, y_train, groups=groups, cv=gss)
-            print("Prefitting Scores:", prefitting_scores)
-            gs = ms.RandomizedSearchCV(estimator=RandomForestClassifier(random_state=42), 
-                                       param_distributions=param_grid, n_iter=30, cv=gss, 
-                                       scoring='accuracy', n_jobs=-1, random_state=random_state)
-            gs.fit(X_train, y_train, groups=groups)
-
-            #train the 'best' estimator and calculate postifitting scores
-            best_rf = gs.best_estimator_
-            best_rf.fit(X_train, y_train)
-            postfitting_scores = ms.cross_val_score(best_rf, X_train, y_train, groups=groups, cv=gss)
-            print("Postfitting Scores:", postfitting_scores)
-            return best_rf
+            model = RandomForestClassifier(random_state=random_state)
         
         case _: #wrong model selected
             return False 
+        
+    #calculate prefitting scores, perform a randomized search; change the n_iter parameter if needed
+    prefitting_scores = ms.cross_val_score(predefined_model, X_train, y_train, groups=groups, cv=gss)
+    print("Prefitting Scores:", prefitting_scores)
+    gs = ms.RandomizedSearchCV(estimator=model, 
+                                param_distributions=param_grid, n_iter=30, cv=gss, 
+                                scoring='accuracy', n_jobs=-1, random_state=random_state)
+    gs.fit(X_train, y_train, groups=groups)
 
+    #train the 'best' estimator and calculate postifitting scores
+    best_model = gs.best_estimator_
+    best_model.fit(X_train, y_train)
+    postfitting_scores = ms.cross_val_score(best_model, X_train, y_train, groups=groups, cv=gss)
+    print("Postfitting Scores:", postfitting_scores)
+    return best_model
 
 def plot_correlation(X,y,name):
     """
     Plots a correlation heatmap and saves it under the given path/name.
 
     arguments: \n
-    X - panda dataframe, which will be examined \n
-    y - panda dataframe, not important
+    :param X: panda dataframe, which will be examined \n
+    :param y: panda dataframe, not important
 
     returns: \n
     None
@@ -259,9 +202,9 @@ def plot_correlation(X,y,name):
     pcv.fit(X,y)
     pcv.transform(X)
     pcv.poof()
-    fig.savefig(name,dpi=300, bbox_inches="tight")
+    fig.savefig(name,dpi=300, format = "svg", bbox_inches="tight")
 
 df = pd.read_csv("segments.csv")
 X_train, y_train, X_test, y_test, groups = prepare_data(df,42)
 #create_profile(X_train)
-train_model(X_train, y_train, groups, "lr",42)
+train_model(X_train, y_train, groups, "nb",42)
